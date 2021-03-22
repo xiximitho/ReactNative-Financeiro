08dac8fc3cb1f833026365af269ec77d5d145c6f
xiximitho <mattesfelipe@gmail.com>
2021-03-21 22:33:34 -0300

    Criado teste de JS utilizando Airbnb

--- App.js
+++ App.js
@@ -7,6 +7,7 @@
 
 import React from 'react';
 import { SafeAreaView, ScrollView, View, Text } from 'react-native';
+import Login from './src/componentes/login';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <ScrollView>
         <View>
           <Text> TTT {4 + ((4 * 96) / 2) * 3}</Text>
+          <Login props1="User222" props2="Senha22" />
         </View>
       </ScrollView>
     </SafeAreaView>
---
+++ src/componentes/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Login = ({ props1, props2 }) => (
+  <View>
+    <Text>
+      Hello Samurai,{props1}, {props2}
+    </Text>
+  </View>
+);
+
+export default Login;
